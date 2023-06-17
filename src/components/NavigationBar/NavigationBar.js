cd59e4acdc24794d270e5f49e2b843ad19d63a3f
EliG <shatikc@gmail.com>
2023-06-17 22:44:50 +0300

    second commit

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -4,20 +4,34 @@ import Avatar from '@mui/material/Avatar';
 import logo from '../../assets/twitterLogo.png';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from '@material-ui/icons/Search';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 const NavigationBar = () => {
   return (
     <div className="navigation-bar">
       <ul>
         <Box display="flex" flexDirection="column" gap={2}>
-          <Avatar className='logo-avatar' sx={{ bgcolor: 'common.white', width: 56, height: 56 }} href="/">
+          <Avatar 
+                  className='logo-avatar' 
+                  sx={{ bgcolor: 'common.white', width: 56, height: 56 }}
+                  href="/">
             <img className="logo-avatar-img" src={logo} alt="Logo"/>
           </Avatar>
-          <Button variant="contained" color="primary" startIcon={<SearchIcon />}>
+          <Button 
+                   className='nav-button'
+                   size='large'
+                   variant="text" 
+                   sx={{ borderRadius: 28, bgcolor: 'transparent', color: 'black', '&:hover': { bgcolor: 'rgba(240, 240, 240)' }}}
+                   startIcon={<SearchIcon />}>
             Explore
           </Button>
-          <Button variant="contained" color="primary" sx={{ borderRadius: 28 }}>
+          <Button 
+                  className='nav-button'
+                  size='large' 
+                  variant="text" 
+                  sx={{ borderRadius: 28, bgcolor: 'transparent', color: 'black', '&:hover': { bgcolor: 'rgba(240, 240, 240)'}}} 
+                  startIcon={<SettingsIcon />}>
             Settings
           </Button>
         </Box>
